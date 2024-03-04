33fab930992c6a44ef4cb830464b64662d114e79
isfelaco <isfelaco@gmail.com>
2024-03-04 09:19:56 -0500

    setup

---
+++ web/www/hw4/homework4.php
@@ -0,0 +1,12 @@
+<?php
+    /**
+     * Homework 4 - PHP Introduction
+     *
+     * Computing ID:isf4rjk
+     * Resources used: [list any resources used to complete this assignment]
+     */
+     
+    // Your functions here
+    
+
+    // No closing php tag needed since there is only PHP in this file
