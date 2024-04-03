10e0088c91d36ce201cfbe3bf4f5bce5a1aa88d4
isfelaco <isfelaco@gmail.com>
2024-04-03 10:38:10 -0400

    login validation

--- web/src/CVille4Rent/components/navbar.php
+++ web/src/CVille4Rent/components/navbar.php
@@ -3,52 +3,38 @@
 <html lang="en">
 
 <head>
-  <!-- url: https://cs4640.cs.virginia.edu/isf4rjk/CVille4Rent -->
-
-  <!-- meta tags -->
   <meta charset="utf-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
-
-  <meta name="author" content="Bella Felaco" />
-  <meta name="description" content="Charlottesville Renter's Application" />
-  <meta name="keywords" content="UVA Charlottesville Rent Renter Renting Apartment Apartments" />
-
-  <!-- title -->
-  <title>CVille 4 Rent: NavBar</title>
 </head>
 
 <body>
-  <nav class="navbar">
+  <nav class="navbar navbar-expand-sm">
     <div class="container-fluid">
       <a class="navbar-brand" href="./">CVille 4 Rent</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      <!-- navbar links -->
+      <div class="collapse navbar-collapse">
+        <div class="navbar-nav me-auto mb-2 mb-lg-0">
           <?php if (empty($_SESSION["user"])): ?>
-            <li class="nav-item">
-              <button class="nav-link" data-bs-toggle="modal" data-bs-target="#modal">Login
-              </button>
-            </li>
+            <button class="nav-link" data-bs-toggle="modal" data-bs-target="#modal">Login
+            </button>
           <?php else: ?>
-            <li>
-              <a class="nav-link" href="?command=profile">
-                Profile
-              </a>
-            </li>
-            <li>
-              <a class="nav-link" href="?command=logout">
-                Logout
-              </a>
-            </li>
+            <a class="nav-link" href="?command=profile">
+              Profile
+            </a>
+            <a class="nav-link" href="?command=logout">
+              Logout
+            </a>
           <?php endif; ?>
-        </ul>
+        </div>
       </div>
     </div>
 
+    <!-- login modal -->
     <div id="modal" class="modal fade" tabindex="-1" aria-hidden="true">
       <div class="modal-dialog">
         <div class="modal-content">
@@ -60,40 +46,67 @@
             <form id="login-form" class="row g-3 needs-validation" novalidate action="?command=login" method="POST">
               <!-- email address input -->
               <div class="form-floating mb-3">
-                <input type="email" class="form-control" id="email" name="email" placeholder="name@example.com">
+                <input type="email" class="form-control" id="email" name="email" placeholder="name@example.com"
+                  required>
                 <label for="email">Email address</label>
-                <!-- validation to be added later -->
                 <div class="invalid-feedback">
-                  No user associated with this email address
+                  Please enter a valid email address
                 </div>
               </div>
 
               <!-- password input -->
               <div class="form-floating">
-                <input type="password" class="form-control" id="password" name="password" placeholder="Password">
+                <input type="password" class="form-control" id="password" name="password" placeholder="Password"
+                  required pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}">
                 <label for="password">Password</label>
-                <!-- validation to be added later -->
                 <div class="invalid-feedback">
-                  Incorrect Password
+                  Please enter a password
+                </div>
+                <div class="invalid-feedback">
+                  <b>Password requirements if creating a new account:</b>
+                  <ul>
+                    <li>At least one digit</li>
+                    <li>At least one lowercase letter</li>
+                    <li>At least one uppercase letter</li>
+                    <li>At least 8 characters long</li>
+                  </ul>
+
                 </div>
               </div>
+
+              <!-- login button - will submit form and authenticate user -->
+              <button id="login-button" class="btn btn-primary" type="submit">
+                Login
+              </button>
             </form>
-          </div>
-          <div class="modal-footer">
-            <!-- login button - will submit form and authenticate user -->
-            <button id="login-button" class="btn btn-primary" type="submit">
-              Login
-            </button>
           </div>
         </div>
       </div>
     </div>
 
   </nav>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+    crossorigin="anonymous"></script>
   <script>
-    document.getElementById('login-button').addEventListener('click', function () {
-      document.getElementById('login-form').submit();
-    });
+    (function () {
+      'use strict'
+      // Fetch all the forms we want to apply custom Bootstrap validation styles to
+      var forms = document.querySelectorAll('.needs-validation');
+
+      // Loop over them and prevent submission
+      Array.prototype.slice.call(forms)
+        .forEach(function (form) {
+          form.addEventListener('submit', function (event) {
+            if (!form.checkValidity()) {
+              event.preventDefault()
+              event.stopPropagation()
+            }
+
+            form.classList.add('was-validated')
+          }, false)
+        });
+    })()
   </script>
 </body>
 
