8f3c5e4d6a5f54992f231794278700a3fa045195
isfelaco <isfelaco@gmail.com>
2024-04-12 17:59:22 -0400

    no reloads + comments

--- web/www/hw6/connections.js
+++ web/www/hw6/connections.js
@@ -68,41 +68,78 @@ let selectedCells = [];
  * Called after startSession and whenever the New Game button is clicked
  */
 function setUpNewGame(newCategories) {
-  // update the number of games played, streak, and total guesses
+  /* update the number of games played, streak, and total guesses */
   const games = JSON.parse(window.localStorage.getItem("games"));
   window.localStorage.setItem("games", JSON.stringify(games + 1));
   const prevWords = JSON.parse(window.localStorage.getItem("words")) || [];
   if (prevWords.length !== 0) window.localStorage.setItem("streak", 0);
-  //   const guesses = JSON.parse(window.localStorage.getItem("guesses"));
-  //   const totalGuesses = window.localStorage.getItem("total-guesses");
-  //   window.localStorage.setItem("total-guesses", totalGuesses + guesses.length);
 
-  // get new categories and words
+  /* get new categories and words */
   const categories = newCategories.categories;
   const words = categories.flatMap((category) => category.words);
   const shuffledWords = shuffleArray(words);
   window.localStorage.setItem("categories", JSON.stringify(categories));
   window.localStorage.setItem("words", JSON.stringify(shuffledWords));
 
-  // reset the guesses and the selectedCells
+  /* reset the guesses and the selectedCells */
   window.localStorage.setItem("guesses", JSON.stringify([]));
   window.localStorage.setItem("selectedCells", JSON.stringify([]));
 
-  // render the updated UI
-  location.reload();
+  /* re-render the UI */
+  renderGame();
+}
+
+/**
+ * Render the game
+ */
+function renderGame() {
+  document.getElementById("guess-button").disabled = selectedCells.length !== 4;
+  document.getElementById("new-game-button").disabled =
+    localStorage.length === 0;
+  document.getElementById("shuffle-button").disabled =
+    JSON.parse(localStorage.getItem("words") || "[]").length === 0;
+
+  const sessionBtn = document.getElementById("session-button");
+  if (localStorage.length === 0) {
+    /* no active session, allow user to start new one */
+    // update the game buttons
+    sessionBtn.textContent = "Start a New Session";
+    sessionBtn.onclick = function () {
+      startSession();
+    };
+    sessionBtn.classList.remove("btn-danger");
+    sessionBtn.classList.add("btn-success");
+  } else {
+    /* active session, allow user to end session */
+    // update the game buttons
+    sessionBtn.textContent = "End Session";
+    sessionBtn.onclick = function () {
+      endSession();
+    };
+    sessionBtn.classList.remove("btn-success");
+    sessionBtn.classList.add("btn-danger");
+
+    // reset the selectedCells
+    window.localStorage.setItem("selectedCells", JSON.stringify([]));
+  }
+  /* re-render the grid, the guesses, and the stats */
+  renderGrid();
+  renderGuessesList();
+  renderStats();
 }
 
 /**
  * Start a new session
  */
 function startSession() {
-  // initialize user stats
+  /* initialize user stats */
   window.localStorage.setItem("games", 0);
   window.localStorage.setItem("wins", 0);
   window.localStorage.setItem("streak", 0);
   window.localStorage.setItem("avg-guesses", 0);
   window.localStorage.setItem("total-guesses", 0);
 
+  /* start a new game */
   getRandomCategories(setUpNewGame);
 }
 
@@ -110,35 +147,26 @@ function startSession() {
  * End the user's current session
  */
 function endSession() {
+  /* clear localStorage */
   window.localStorage.clear();
-  location.reload();
-}
 
-/**
- * Shuffle an array
- */
-function shuffleArray(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    // Generate a random index between 0 and i (inclusive)
-    const j = Math.floor(Math.random() * (i + 1));
-
-    // Swap elements array[i] and array[j]
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-
-  return array;
+  /* re-render the UI */
+  renderGame();
 }
 
 /**
  * Render a 4x4 grid of the words
  */
 function renderGrid() {
-  const words = JSON.parse(window.localStorage.getItem("words"));
-
   const table = document.getElementById("words-grid");
   table.innerHTML = "";
 
-  if (words.length > 0) {
+  if (localStorage.length !== 0) {
+    const words = JSON.parse(window.localStorage.getItem("words"));
+    if (words.length === 0) {
+      table.innerHTML = "<h1>Game Over!</h1>";
+      return;
+    }
     for (let i = 0; i < 4; i++) {
       const row = document.createElement("tr");
       for (let j = 0; j < 4; j++) {
@@ -154,19 +182,7 @@ function renderGrid() {
       }
       table.appendChild(row);
     }
-  } else {
-    table.innerHTML = "<h1>Game Over!</h1>";
   }
-}
-
-/**
- * Shuffle the board
- */
-function shuffleBoard() {
-  const words = JSON.parse(window.localStorage.getItem("words"));
-  const shuffledWords = shuffleArray(words);
-  window.localStorage.setItem("words", JSON.stringify(shuffledWords));
-  renderGrid();
 }
 
 /**
@@ -212,7 +228,7 @@ function handleGuess() {
     const catWords = category.words;
 
     let curCount = 0;
-    // count the number of correct words in the guess for the current category
+    /* count the number of correct words in the guess for the current category */
     selectedCells.forEach((word) => {
       if (catWords.includes(word)) {
         curCount++;
@@ -222,7 +238,7 @@ function handleGuess() {
     if (curCount === 1) curCount = 0;
     if (curCount > matchCount) matchCount = curCount;
 
-    // check if the entire guess matches the category
+    /* check if the entire guess matches the category */
     if (curCount === 4) {
       // remove the correctly guessed words from the grid
       filteredWords = words.filter((word) => !selectedCells.includes(word));
@@ -231,47 +247,53 @@ function handleGuess() {
       break;
     }
   }
-  // add the guess to the array of guesses
+
+  /* add the guess to the array of guesses */
   const guesses = JSON.parse(window.localStorage.getItem("guesses"));
   guesses.push({ guess: [...selectedCells], matchCount });
   window.localStorage.setItem("guesses", JSON.stringify(guesses));
 
-  // reset the selectedCells
+  /* reset the selectedCells */
   if (matchCount === 4) selectedCells = [];
   window.localStorage.setItem("selectedCells", JSON.stringify(selectedCells));
 
-  // if there are no words left, the game is over
+  /* if there are no words left, the game is over */
   if (filteredWords.length === 0) handleWin();
 
-  // update the page
-  location.reload();
+  /* re-render the UI */
+  renderGame();
 }
 
 /**
  * Perform actions if user wins a game
  */
 function handleWin() {
+  /* increment the number of wins */
   var wins = JSON.parse(window.localStorage.getItem("wins"));
   window.localStorage.setItem("wins", JSON.stringify(wins + 1));
 
+  /* increment the streak */
   var streak = JSON.parse(window.localStorage.getItem("streak"));
   window.localStorage.setItem("streak", JSON.stringify(streak + 1));
 
-  updateAvgGuesses();
-}
-
-/**
- * Update the average guesses per win
- */
-function updateAvgGuesses() {
+  /* update avg-guesses */
   const games = JSON.parse(window.localStorage.getItem("games"));
   const curGuesses = JSON.parse(window.localStorage.getItem("guesses"));
+
+  // add curGuesses to get totalGuesses
   const totalGuesses =
     JSON.parse(window.localStorage.getItem("total-guesses")) +
     curGuesses.length;
+
+  // set the new total-guesses
   window.localStorage.setItem("total-guesses", JSON.stringify(totalGuesses));
+
+  // calculate and set the new avg-guesses
   const avgGuesses = totalGuesses / games;
   window.localStorage.setItem("avg-guesses", avgGuesses.toFixed(2));
+
+  /* re-render the UI */
+  renderGame();
 }
 
 /**
@@ -279,41 +301,41 @@ function updateAvgGuesses() {
  */
 function renderGuessesList() {
   const list = document.getElementById("guesses-list");
-  list.innerHTML = ""; // Clear previous content
+  list.innerHTML = "";
 
-  // Retrieve the current guesses from localStorage
   const guesses = JSON.parse(window.localStorage.getItem("guesses")) || [];
 
-  // Iterate over each guess and create a list item
-  guesses.forEach((guessObj) => {
-    const { guess, matchCount } = guessObj;
+  if (guesses.length !== 0) {
+    guesses.forEach((guessObj) => {
+      const { guess, matchCount } = guessObj;
 
-    const guessItem = document.createElement("li");
-    guessItem.classList.add("list-group-item");
-    guessItem.textContent = guess.join(", "); // Display guess as comma-separated string
+      const guessItem = document.createElement("li");
+      guessItem.classList.add("list-group-item");
+      guessItem.textContent = guess.join(", ");
 
-    // add a badge for number of correct words
-    const badge = document.createElement("span");
-    let badgeClass = "";
-    switch (matchCount) {
-      case 2:
-      case 3:
-        badgeClass = "bg-warning";
-        break;
-      case 4:
-        badgeClass = "bg-success";
-        break;
-      default:
-        badgeClass = "bg-danger";
-        break;
-    }
-    badge.classList.add("badge", badgeClass);
-    badge.textContent = matchCount;
-    guessItem.appendChild(document.createTextNode(" "));
-    guessItem.appendChild(badge);
+      /* add a badge for number of correct words */
+      const badge = document.createElement("span");
+      let badgeClass = "";
+      switch (matchCount) {
+        case 2:
+        case 3:
+          badgeClass = "bg-warning";
+          break;
+        case 4:
+          badgeClass = "bg-success";
+          break;
+        default:
+          badgeClass = "bg-danger";
+          break;
+      }
+      badge.classList.add("badge", badgeClass);
+      badge.textContent = matchCount;
+      guessItem.appendChild(document.createTextNode(" "));
+      guessItem.appendChild(badge);
 
-    list.appendChild(guessItem);
-  });
+      list.appendChild(guessItem);
+    });
+  }
 }
 
 /**
@@ -323,26 +345,54 @@ function renderStats() {
   const list = document.getElementById("user-stats");
   list.innerHTML = "";
 
-  const gamesItem = document.createElement("li");
-  gamesItem.classList.add("list-group-item");
-  gamesItem.textContent =
-    "Games Played: " + window.localStorage.getItem("games");
-  list.appendChild(gamesItem);
+  if (localStorage.length !== 0) {
+    const gamesItem = document.createElement("li");
+    gamesItem.classList.add("list-group-item");
+    gamesItem.textContent =
+      "Games Played: " + window.localStorage.getItem("games");
+    list.appendChild(gamesItem);
 
-  const winsItem = document.createElement("li");
-  winsItem.classList.add("list-group-item");
-  winsItem.textContent = "Games Won: " + window.localStorage.getItem("wins");
-  list.appendChild(winsItem);
+    const winsItem = document.createElement("li");
+    winsItem.classList.add("list-group-item");
+    winsItem.textContent = "Games Won: " + window.localStorage.getItem("wins");
+    list.appendChild(winsItem);
 
-  const streakItem = document.createElement("li");
-  streakItem.classList.add("list-group-item");
-  streakItem.textContent =
-    "Current Streak: " + window.localStorage.getItem("streak");
-  list.appendChild(streakItem);
+    const streakItem = document.createElement("li");
+    streakItem.classList.add("list-group-item");
+    streakItem.textContent =
+      "Current Streak: " + window.localStorage.getItem("streak");
+    list.appendChild(streakItem);
 
-  const avgGuessesItem = document.createElement("li");
-  avgGuessesItem.classList.add("list-group-item");
-  avgGuessesItem.textContent =
-    "Average Guesses per Win: " + window.localStorage.getItem("avg-guesses");
-  list.appendChild(avgGuessesItem);
+    const avgGuessesItem = document.createElement("li");
+    avgGuessesItem.classList.add("list-group-item");
+    avgGuessesItem.textContent =
+      "Average Guesses per Win: " + window.localStorage.getItem("avg-guesses");
+    list.appendChild(avgGuessesItem);
+  }
+}
+
+/**
+ * Shuffle an array
+ */
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
+/**
+ * Shuffle the board
+ */
+function shuffleBoard() {
+  const words = JSON.parse(window.localStorage.getItem("words"));
+  const shuffledWords = shuffleArray(words);
+  window.localStorage.setItem("words", JSON.stringify(shuffledWords));
+
+  /* reset selectedCells */
+  window.localStorage.setItem("selectedCells", JSON.stringify([]));
+
+  /* re-render the UI */
+  renderGrid();
 }
